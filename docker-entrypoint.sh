28177d996fb4758445a32b03aa9254692593df75
Kevine-fr <124605694+Kevine-fr@users.noreply.github.com>
2024-08-20 01:16:24 +0200

    Update docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,16 +1,13 @@
-#!/bin/sh
+#!/bin/bash
 
-# Attendre que la base de données soit prête
-echo "Waiting for database..."
-until nc -z -v -w30 db 3306
-do
-  echo "Waiting for database connection..."
-  sleep 15
-done
+# Attendre que MySQL soit prêt
+echo "Waiting for MySQL to be ready..."
+/usr/local/bin/wait-for-it.sh mysql:3306 --timeout=60 --strict -- echo "MySQL is up"
 
-echo "Database is up - executing migrations"
-# Exécuter les migrations
+# Exécuter les migrations avec force
+echo "Running migrations..."
 php artisan migrate --force
 
 # Démarrer le serveur Laravel
-php artisan serve --host=0.0.0.0 --port=8000
+echo "Starting Laravel server..."
+exec php artisan serve --host=0.0.0.0 --port=8000
