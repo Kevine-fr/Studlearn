370d7baecc894988b7eced09b5fa6a24768e0c26
Kevine-fr <124605694+Kevine-fr@users.noreply.github.com>
2024-08-20 17:20:09 +0200

    Update docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,10 +1,16 @@
 #!/bin/sh
 
-# Lance le serveur PHP en arrière-plan
+# Ajouter node_modules/.bin au PATH
+export PATH=/var/www/html/node_modules/.bin:$PATH
+
+# Installer les dépendances Node.js (au cas où elles n'auraient pas été installées)
+npm install
+
+# Lancer le serveur PHP en arrière-plan
 php artisan serve --host=0.0.0.0 --port=8000 &
 
-# Lance les tâches npm
+# Lancer les tâches npm
 npm run dev &
 
-# Attend que tous les processus en arrière-plan se terminent pour garder le conteneur en cours d'exécution
+# Garder le conteneur en cours d'exécution
 wait
